c60cc6447b25930d8278e33aafa08abfbdfd5f89
Moinuddin-dotcom <moinchy7@gmail.com>
2025-04-16 00:53:26 +0600

    error lotti update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,29 +1,24 @@
-// 'use client'
-// // import Lottie from 'lottie-react'
-// import Link from 'next/link'
+
+// import Lottie from 'lottie-react'
+import Link from 'next/link'
 // import errorLotti from '@/public/assets/Error-Animation.json'
 // import dynamic from 'next/dynamic'
 
 
 
-// // Dynamically import Lottie with SSR disabled
+// Dynamically import Lottie with SSR disabled
 // const Lottie = dynamic(() => import('lottie-react'), { ssr: false })
 
-// export default function NotFoundPage404() {
-//     return (
-//         <div className='max-w-[35vw] mx-auto'>
-//             <Lottie animationData={errorLotti}></Lottie>
-//             <div className='text-center'>
-//                 <Link href={'/'} className='btn'>Go Back To <span className='font-semibold text-blue-400 text-lg underline underline-offset-4'>Home Page</span></Link>
-//             </div>
-//         </div>
-//     )
-// }
-
-
-import NotFoundClient from "@/components/NotFoundClient";
-
-
-export default function NotFoundPage() {
-    return <NotFoundClient />
+export default function NotFoundPage404() {
+    return (
+        <div className='max-w-[35vw] mx-auto'>
+            {/* <Lottie animationData={errorLotti}></Lottie> */}
+            <h1 className='text-center'>This is a Error Page</h1>
+            <div className='text-center'>
+                <Link href={'/'} className='btn'>Go Back To <span className='font-semibold text-blue-400 text-lg underline underline-offset-4'>Home Page</span></Link>
+            </div>
+        </div>
+    )
 }
+
+
