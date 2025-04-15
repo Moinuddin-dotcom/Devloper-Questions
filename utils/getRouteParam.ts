7ed570a63c045f7a8345ac707897b207e10958af
Moinuddin-dotcom <moinchy7@gmail.com>
2025-04-16 00:21:30 +0600

    api changing to ts to js

--- utils/getRouteParam.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server";
-
-export function getRouteParam(req: NextRequest, pattern: string, paramName: string): string | null {
-    const urlPattern = new URLPattern({ pathname: pattern })
-    const match = urlPattern.exec(req.nextUrl.pathname)
-    return match?.pathname.groups[paramName] || null
-}
