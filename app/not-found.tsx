1d31575de2befd1d04256415266f6546e3a6c2d0
Moinuddin-dotcom <moinchy7@gmail.com>
2025-04-16 00:41:47 +0600

    error lotti update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,32 @@
-'use client'
-// import Lottie from 'lottie-react'
-import Link from 'next/link'
-import errorLotti from '@/public/assets/Error-Animation.json'
+// 'use client'
+// // import Lottie from 'lottie-react'
+// import Link from 'next/link'
+// import errorLotti from '@/public/assets/Error-Animation.json'
+// import dynamic from 'next/dynamic'
+
+
+// // Dynamically import Lottie with SSR disabled
+// const Lottie = dynamic(() => import('lottie-react'), { ssr: false })
+
+// export default function NotFoundPage404() {
+//     return (
+//         <div className='max-w-[35vw] mx-auto'>
+//             <Lottie animationData={errorLotti}></Lottie>
+//             <div className='text-center'>
+//                 <Link href={'/'} className='btn'>Go Back To <span className='font-semibold text-blue-400 text-lg underline underline-offset-4'>Home Page</span></Link>
+//             </div>
+//         </div>
+//     )
+// }
+
+
 import dynamic from 'next/dynamic'
 
-// Dynamically import Lottie with SSR disabled
-const Lottie = dynamic(() => import('lottie-react'), { ssr: false })
+const NotFoundClient = dynamic(() => import('@/components/NotFoundClient'), {
+    ssr: false,
+    loading: () => <p>Loading...</p>
+})
 
-export default function NotFoundPage404() {
-    return (
-        <div className='max-w-[35vw] mx-auto'>
-            <Lottie animationData={errorLotti}></Lottie>
-            <div className='text-center'>
-                <Link href={'/'} className='btn'>Go Back To <span className='font-semibold text-blue-400 text-lg underline underline-offset-4'>Home Page</span></Link>
-            </div>
-        </div>
-    )
+export default function NotFoundPage() {
+    return <NotFoundClient />
 }
