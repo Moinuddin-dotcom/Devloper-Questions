61a3e34081b1323c0c478ce4b7826cf85e1dbf3a
Moinuddin-dotcom <moinchy7@gmail.com>
2025-04-16 01:07:59 +0600

    loading update

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,8 @@
-'use client'
-import Lottie from "lottie-react";
-import loadingLottie from '@/public/assets/Loading-Animation.json'
+
 export default function Loading() {
     return (
         <div>
-            <Lottie animationData={loadingLottie}></Lottie>
+            <h1>Loading...................</h1>
         </div>
     )
 }
