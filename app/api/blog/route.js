22df890bdd893116a6a3d84225d02df1f65c640e
Moinuddin-dotcom <moinchy7@gmail.com>
2025-04-16 01:53:10 +0600

    url updated

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -3,25 +3,23 @@ import dbConnect, { collectionNameObj } from "@/lib/dbConnect"
 // import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 
-// Getting all question collection data
+// Getting all blogCollection data
 export const GET = async () => {
     // const session = await getServerSession(authOptions)
     // if (session) {
     // const email = session?.user?.email // Uncomment this line if you need the email for filtering
-    const questionCollection = dbConnect(collectionNameObj.questionCollection)
-    const result = await questionCollection.find({}).toArray()
+    const blogCollection = await dbConnect(collectionNameObj.blogCollection)
+    const result = await blogCollection.find({}).toArray()
     return NextResponse.json(result)
     // }
 
     // return NextResponse.json({})
 }
 
-// posting on questionCollection
-export const POST = async (req: Request) => {
-
+// Posting to blogCollection
+export const POST = async (req) => {
     const body = await req.json()
-    const questionCollection = dbConnect(collectionNameObj.questionCollection)
-    const result = await questionCollection.insertOne(body)
+    const blogCollection = await dbConnect(collectionNameObj.blogCollection)
+    const result = await blogCollection.insertOne(body)
     return NextResponse.json(result)
 }
-
--- app/api/question/route.js
+++ app/api/question/route.js
@@ -3,24 +3,23 @@ import dbConnect, { collectionNameObj } from "@/lib/dbConnect"
 // import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 
-// Getting all blogcollection data
+// Getting all question collection data
 export const GET = async () => {
     // const session = await getServerSession(authOptions)
     // if (session) {
     // const email = session?.user?.email // Uncomment this line if you need the email for filtering
-    const blogCollection = await dbConnect(collectionNameObj.blogCollection)
-    const result = await blogCollection.find({}).toArray()
+    const questionCollection = await dbConnect(collectionNameObj.questionCollection)
+    const result = await questionCollection.find({}).toArray()
     return NextResponse.json(result)
     // }
 
     // return NextResponse.json({})
 }
 
-// posting on blogCollection
-export const POST = async (req: Request) => {
-
+// Posting to questionCollection
+export const POST = async (req) => {
     const body = await req.json()
-    const blogCollection = await dbConnect(collectionNameObj.blogCollection)
-    const result = await blogCollection.insertOne(body)
+    const questionCollection = await dbConnect(collectionNameObj.questionCollection)
+    const result = await questionCollection.insertOne(body)
     return NextResponse.json(result)
 }
